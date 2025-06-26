981ab25992ef9158d8f7cd39f3a34b39b2484b2d
Solange Gamboa <solange.gamboa@gmail.com>
2025-06-26 19:07:15 +0000

    use o .env.local para pegar a minha Gemini API Key e manter as variáveis

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,12 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import 'dotenv/config';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GOOGLE_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
