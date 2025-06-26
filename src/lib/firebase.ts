01e93878ec0e1e3b4dc1441a78f4e2e7cd871acf
Solange Gamboa <solange.gamboa@gmail.com>
2025-06-26 18:58:05 +0000

    verifique se o firestore databse está configurado para conectar com o ba

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,6 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getFirestore(app, 'solarfin');
 
 export { app, auth, db };
