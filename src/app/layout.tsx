6a82f7ec56b12daea6ed54c12141dbffa88694e9
Solange Gamboa <solange.gamboa@gmail.com>
2025-07-01 21:59:33 +0000

    unhandledRejection [Error [PageNotFoundError]: Cannot find module for pa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-      </head>
+      <head />
       <body className={`${ptSans.variable} font-body antialiased`}>
         <ThemeProvider
           attribute="class"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,14 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['var(--font-sans)', 'sans-serif'],
+        headline: ['var(--font-sans)', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
